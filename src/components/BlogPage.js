b720160c8edbf124a17d2852d30bef26519d16f4
kakeru1881 <hamiribu.kakeru@gmail.com>
2023-07-22 17:13:18 +0900

    first commit

---
+++ src/components/BlogPage.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function BlogPage() {
+  return (
+    <div className ="container">
+      <h1>ブログ</h1>
+      <p>North Korea,
+        officially the Democratic People's Republic of Korea ,
+        is a country in East Asia. It constitutes the northern half of the Korean 
+        Peninsula and borders China and Russia to the north at the Yalu and Tumen rivers, 
+        and South Korea to the south at the Korean Demilitarized Zone. North Korea's border with 
+        South Korea is a disputed border as both countries claim the entirety of the Korean Peninsula. 
+        The country's western border is formed by the Yellow Sea, while its eastern border is defined by 
+        the Sea of Japan. North Korea, like its southern counterpart, claims to be the 
+        legitimate government of the entire peninsula and adjacent islands. 
+        Pyongyang is the capital and largest city.
+        </p>
+      <p>North Korea,
+        officially the Democratic People's Republic of Korea ,
+        is a country in East Asia. It constitutes the northern half of the Korean
+        Peninsula and borders China and Russia to the north at the Yalu and Tumen rivers,
+        and South Korea to the south at the Korean Demilitarized Zone. North Korea's border with
+        South Korea is a disputed border as both countries claim the entirety of the Korean Peninsula.
+        The country's western border is formed by the Yellow Sea, while its eastern border is defined by
+        the Sea of Japan. North Korea, like its southern counterpart, claims to be the
+        legitimate government of the entire peninsula and adjacent islands.
+        Pyongyang is the capital and largest city.
+      </p>
+      <p>North Korea,
+        officially the Democratic People's Republic of Korea ,
+        is a country in East Asia. It constitutes the northern half of the Korean
+        Peninsula and borders China and Russia to the north at the Yalu and Tumen rivers,
+        and South Korea to the south at the Korean Demilitarized Zone. North Korea's border with
+        South Korea is a disputed border as both countries claim the entirety of the Korean Peninsula.
+        The country's western border is formed by the Yellow Sea, while its eastern border is defined by
+        the Sea of Japan. North Korea, like its southern counterpart, claims to be the
+        legitimate government of the entire peninsula and adjacent islands.
+        Pyongyang is the capital and largest city.
+      </p>
+    </div>
+  )
+}
+
+export default BlogPage
